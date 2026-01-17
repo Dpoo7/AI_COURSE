912d1664362df0cf75058563ac6ecc8cde48582e
Divy Prajapati <divyprajapati0799@gmail.com>
2026-01-17 14:44:10 +0530

    Add files via upload

---
+++ DAY 4/my_list.py
@@ -0,0 +1,15 @@
+nums = [1,2,3,4,5]
+
+#nums.appends()
+print(nums)
+add_nums = [6,7,8]
+
+for num in add_nums:
+    #print(nums)
+    nums.append(num)
+print(nums)
+
+nums.extend([11,12,13])
+
+print(nums)
+
